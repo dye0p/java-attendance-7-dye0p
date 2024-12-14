1fea4e1b2d02e8757ea98148f590dc97befe2cea
DongYeop <s01054830455@gmail.com>
2024-12-14 14:20:52 +0900

    feat: 출석 확인 기능 구현

--- src/main/java/attendance/view/OutputView.java
+++ src/main/java/attendance/view/OutputView.java
@@ -2,8 +2,19 @@ package attendance.view;
 
 public class OutputView {
 
+    private static final String NEXT_LINE = System.lineSeparator();
+
     public void printToday(int monthValue, int dayOfMonth, String dayOfWeek) {
         String format = String.format("오늘은 %d월 %d일 %s입니다. 기능을 선택해 주세요", monthValue, dayOfMonth, dayOfWeek);
         System.out.println(format);
+    }
+
+    public void printTodayAttendanceResult(int monthValue, int dayOfMonth, String dayOfWeek, String goTime,
+                                           String check) {
+        String format = String.format("%d월 %d일 %s %s %s", monthValue, dayOfMonth, dayOfWeek, goTime,
+                check);
+
+        System.out.println(NEXT_LINE + format);
+
     }
 }
