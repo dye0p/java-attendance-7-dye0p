e850a52ed740324abefa4b941d0b05bfb459d443
DongYeop <s01054830455@gmail.com>
2024-12-14 14:46:11 +0900

    fix: 출석부 기록 인덱스 버그 수정

--- src/main/java/attendance/controller/AttendanceController.java
+++ src/main/java/attendance/controller/AttendanceController.java
@@ -14,6 +14,7 @@ public class AttendanceController {
 
     private final InputView inputView;
     private final OutputView outputView;
+    private Attendances attendances;
 
     public AttendanceController(InputView inputView, OutputView outputView) {
         this.inputView = inputView;
@@ -23,7 +24,7 @@ public class AttendanceController {
     public void run() {
         CrewFileReader crewFileReader = new CrewFileReader();
         List<Attendance> attendanceList = crewFileReader.attendancesReader();
-        Attendances attendances = new Attendances(attendanceList);
+        attendances = new Attendances(attendanceList);
 
         //오늘의 날짜를 가져옴.
         LocalDateTime now = DateTimes.now();
@@ -38,55 +39,83 @@ public class AttendanceController {
 
         //기능이 1번인 경우
         if (option.equals("1")) {
+            optionOne(dayOfWeek, monthValue, dayOfMonth, attendances);
+        }
 
-            //등교 날짜인지 확인
-            if (dayOfWeek.equals("토요일") || dayOfWeek.equals("일요일")) {
-                String format = String.format("[ERROR] %d월 %d일 %s은 등교일이 아닙니다.", monthValue, dayOfMonth, dayOfWeek);
-                throw new IllegalArgumentException(format);
-            }
+        //기능 2번인 경우
+        if (option.equals("2")) {
 
-            //등교 날짜라면
+            //출석을 수정하려는 닉네임
+            String name = inputView.readUpdateNickName();
 
-            //닉네임 입력
-            String name = inputView.readNickName();
-
-            //닉네임 존재 확인
+            //닉네임이 존재하는지 확인한다.
             attendances.isContain(name);
 
-            //등교 시간 입력
-            String goTime = inputView.readGoTime();
+            //수정하려는 날짜 입력
+            String date = inputView.updateDate();
 
-            //등교시간 유효성 검사
-            String[] goTimeSplit = goTime.split(":");
+            //해당 닉네임에 대한 일의 출석이 존재하는지 확인한다.
+            attendances.findAttendanceBy(name, date);
+        }
+    }
 
-            if (goTimeSplit.length != 2) {
-                throw new IllegalArgumentException("[ERROR] 잘못된 형식을 입력하였습니다.");
-            }
+    private void optionOne(String dayOfWeek, int monthValue, int dayOfMonth, Attendances attendances) {
+        //등교 날짜인지 확인
+        if (dayOfWeek.equals("토요일") || dayOfWeek.equals("일요일")) {
+            String format = String.format("[ERROR] %d월 %d일 %s은 등교일이 아닙니다.", monthValue, dayOfMonth, dayOfWeek);
+            throw new IllegalArgumentException(format);
+        }
 
-            //시간 범위를 초과하면
-            if (Integer.parseInt(goTimeSplit[0]) > 24 || Integer.parseInt(goTimeSplit[1]) > 60) {
-                throw new IllegalArgumentException("[ERROR] 잘못된 형식을 입력하였습니다.");
+        //등교 날짜라면
 
-            }
+        //닉네임 입력
+        String name = inputView.readNickName();
 
-            //등교 가능 날짜와 가능 시간이라면 출석처리
-            //월요일이라면 13시 이후 부터 지각,초과 처리
-            //화~금 이라면 10시 이후 부터 지각,초과 처리
-            if (dayOfWeek.equals("월요일")) {
-                //13:00 분 안에 출석인지
-                String check = "출석";
-                if (Integer.parseInt(goTimeSplit[0]) <= 13 && Integer.parseInt(goTimeSplit[1]) <= 0) {
-                    outputView.printTodayAttendanceResult(monthValue, dayOfMonth, dayOfWeek, goTime, check);
-                }
-            }
+        //닉네임 존재 확인
+        attendances.isContain(name);
 
-            if (!dayOfWeek.equals("월요일")) {
-                //10:00분 안에 출석인지
-                String check = "(출석)";
-                if (Integer.parseInt(goTimeSplit[0]) <= 10 && Integer.parseInt(goTimeSplit[1]) <= 0) {
-                    outputView.printTodayAttendanceResult(monthValue, dayOfMonth, dayOfWeek, goTime, check);
-                }
+        //등교 시간 입력
+        String goTime = inputView.readGoTime();
+
+        //등교시간 유효성 검사
+        String[] goTimeSplit = goTime.split(":");
+
+        if (goTimeSplit.length != 2) {
+            throw new IllegalArgumentException("[ERROR] 잘못된 형식을 입력하였습니다.");
+        }
+
+        //시간 범위를 초과하면
+        if (Integer.parseInt(goTimeSplit[0]) > 24 || Integer.parseInt(goTimeSplit[1]) > 60) {
+            throw new IllegalArgumentException("[ERROR] 잘못된 형식을 입력하였습니다.");
+
+        }
+
+        //등교 가능 날짜와 가능 시간이라면 출석처리
+        //월요일이라면 13시 이후 부터 지각,초과 처리
+        //화~금 이라면 10시 이후 부터 지각,초과 처리
+        if (dayOfWeek.equals("월요일")) {
+            //13:00 분 안에 출석인지
+            String check = "출석";
+            if (Integer.parseInt(goTimeSplit[0]) <= 13 && Integer.parseInt(goTimeSplit[1]) <= 0) {
+                outputView.printTodayAttendanceResult(monthValue, dayOfMonth, dayOfWeek, goTime, check);
             }
         }
+
+        if (!dayOfWeek.equals("월요일")) {
+            //10:00분 안에 출석인지
+            String check = "(출석)";
+            if (Integer.parseInt(goTimeSplit[0]) <= 10 && Integer.parseInt(goTimeSplit[1]) <= 0) {
+                outputView.printTodayAttendanceResult(monthValue, dayOfMonth, dayOfWeek, goTime, check);
+            }
+        }
+
+        //출석이 완료되면
+        //출석부에 기록함
+
+        int hour = Integer.parseInt(goTimeSplit[0]);
+        int minute = Integer.parseInt(goTimeSplit[1]);
+
+        Attendance attendance = new Attendance(name, 2024, monthValue, dayOfMonth, hour, minute);
+        attendances.updateAttendance(attendance);
     }
 }
